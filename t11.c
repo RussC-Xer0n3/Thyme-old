cb0c99e4bca648fe5086f6a3fd0fa60082d98157
Xer0n3 - RussC <developing821@outlook.com>
2022-06-04 16:46:53 +0100

    Create t11.c
    Some heavy modification to notes and how the code is organised

---
+++ t11.c
@@ -0,0 +1,98 @@
+/**
+ * Learn from the probability of time and question the probability.
+ * An expansion from merging Thyme project with QBits and Particulates
+ * @Created and finished between 04.09.2020 and 05.09.2020
+ * @Listening Peace Orchestra.
+ * @By Russell A E Clarke Et. al
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <time.h>
+
+float A;                // Spherical calculation based on PI, DIM and RAD
+float T;                // Time, dependencies are: V, sum PT,FT, DIM and IN  
+float C;                // is the SIGNAL for dependencies of A and IN
+float PT;               // Past Time dependencies are: REMAINDER of future time over the constant divisible over T
+float FT;               // Future Time dependencies are: Time divided over the constant subtracted from PT
+float V;                // constant based on 3d space time
+float PI;               // PI is Pi to 10 decimal places
+float DIM;              //could be 3rd dimension for example. Set the value here or in the method, Method preferred.
+float SIG;              // Is a generic sigmoid function modified for a unique sigal, perhaps we will make that NTP port 143
+float VEL;              // should eventually return an array with velocities per calculation
+float IN;               // Input        
+float RAD = 1;          // Radius set to 1 for now
+
+float WEIGHT;           // Function for randomly generating weights
+float weights;          // all weights
+float EPSILON;          // A small positive number 
+float SIGMOID;          // Generic sigmoid function
+float COST;             // cost of efficiency in network
+float BIAS;             // Function for randomly generating biases
+float REWARD;           //AI reward for RL
+float PROBABILITY;      // probability of x occurring in y environment over z frames
+float MDP;              // Markov Decision Process, dependencies: Reward, Probability
+float Q;                // My XOR logic for MDP and RL
+float R;                // Probability of occurence dependencies are REWARD, BIAS, Q
+float INPUT;            // All inputs
+float HIDDEN;           // Hidden NN layer(s)
+float OUTPUT;           // All outputs
+
+float thyme (float argc, char *argv[]) {
+
+    //The basics, learning rate first
+    #define EPSILON 5E-5
+    #define Y (*: 0 ? 1)
+    #define PI 3.1415926535
+    #define A (pow((PI * RAD), DIM))
+    #define SIG ((1) / (-1(C--)))
+    #define SIGMOID (1.0 / math.sum(-1, (exp(O)--)))
+    
+    //Preparing for NN and Biases
+    #define WEIGHT (modf((math.sum(randn() % 1.0), 0.0), float *wdiscard))
+    #define BIAS (modf((math.sum(randn() % 1.0), 0.0), float *bdiscard))
+    
+    //Weigh up the costs and gradient of descent
+    #define COST (weights, BIAS)
+    #define GRAD_DESCENT (math.pow(((COST) - (COST(EPSILON))), 2))
+        
+    //Define constant first
+    #define C SIG(math.exp(math.pow(A), ((math.exp(IN++)) / (math.exp(--IN))))) //predecrement all time as past time divide over increment of all time for continuum. Had to swap them as per order of operation
+    //#define C SIG(exp(pow(A), ((exp(++PT)) / (exp(FT++))))) //preincrement the past and divide over the future
+    
+    //Begin telling computer about the past and future and all time
+    #define PT ((FT % C) / T)
+    #define FT (((T) / C) - PT)
+    #define T (math.exp((V / math.sum(PT, FT)) / (math.exp(math.pow(IN, DIM), (V)))))
+    
+    //Tell it what to calculate and what is going into the constant
+    #define V (math.pow(T, 3) / (PT - FT))
+    #define VEL [T, V]
+    #define IN T
+
+    //Markov Decision Process
+    #define REWARD ((INPUT, N) / Y)
+    #define PROBABILITY ((INPUT, Y) / N)
+    #define MDP (math.sum(REWARD, BIAS) / PROBABILITY)
+
+    //Implementation
+    #define N INPUT++
+    #define OUT SIGMOID((weights * N) + BIAS)
+    
+    //Calculate the probability
+    #define Q math.pow(math.pow(MDP, N), 2) + Y
+    #define R ((REWARD + BIAS) / Q)
+
+    //Initialise the input, output and hidden layers of the network 
+    #define INPUT ((T, OUT, OUTPUT) * WEIGHT) + BIAS
+    #define HIDDEN (((10 * math.pow(20, 4)) * WEIGHT * (INPUT))
+    #define OUTPUT math.sum(((4 * WEIGHT) / (HIDDEN)), BIAS)
+
+    #define X (Q*(PI*A)))
+    #define PARTIAL ((X, Q++) / Y)
+    #define PARTIALMDP (math.sum(REWARD, BIAS) / PARTIAL)
+    
+    return 1;
+}
